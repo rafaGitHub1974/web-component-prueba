43fb1290ca1d8fc6004dab3aa5a97c12c4607d0d
Perez Aliende, Rafael <rpereza@umivaleactiva.es>
2024-04-09 17:41:41 +0200

    delete ficheros innecesarios

--- src/plugins/ui/confirmationService.ts
+++
@@ -1,9 +0,0 @@
-import ConfirmationService from 'primevue/confirmationservice'
-import { App } from 'vue'
-
-export default {
-  install: (app: App<Element>) => {
-    const vue = app
-    vue.use(ConfirmationService)
-  }
-}
--- src/plugins/ui/toast.ts
+++
@@ -1,9 +0,0 @@
-import ToastService from 'primevue/toastservice'
-import { App } from 'vue'
-
-export default {
-  install: (app: App<Element>) => {
-    const vue = app
-    vue.use(ToastService)
-  }
-}
